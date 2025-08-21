6f6f77cc8402294ad3be29477b9b61e30cce546d
gleebuild <gleebuild@gmail.com>
2025-08-21 13:30:36 +0000

    Delete app directory

--- app/controllers/discourse_shop/admin/base_controller.rb
+++
@@ -1,7 +0,0 @@
-
-# frozen_string_literal: true
-class DiscourseShop::Admin::BaseController < ::ApplicationController
-  requires_plugin ::DiscourseShop::PLUGIN_NAME
-  before_action :ensure_staff
-  skip_before_action :check_xhr
-end
