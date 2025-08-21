e07d81f171b559b82bf09e75a9d626f24959b23d
gleebuild <gleebuild@gmail.com>
2025-08-21 13:47:51 +0000

    Delete assets directory

--- assets/javascripts/discourse/routes/shop-index.js
+++
@@ -1,9 +0,0 @@
-
-import DiscourseRoute from "discourse/routes/discourse";
-import { ajax } from "discourse/lib/ajax";
-
-export default class ShopIndexRoute extends DiscourseRoute {
-  model() {
-    return ajax("/shop-api/public/products.json");
-  }
-}
--- assets/javascripts/discourse/routes/shop-product.js
+++
@@ -1,9 +0,0 @@
-
-import DiscourseRoute from "discourse/routes/discourse";
-import { ajax } from "discourse/lib/ajax";
-
-export default class ShopProductRoute extends DiscourseRoute {
-  model(params) {
-    return ajax(`/shop-api/public/products/${params.id}.json`);
-  }
-}
