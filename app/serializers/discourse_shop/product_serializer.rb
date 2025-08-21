30c97f6a258c34e17d8fd0adf8ba91a66b1eaaca
gleebuild <gleebuild@gmail.com>
2025-08-21 13:09:24 +0000

    Delete app directory

--- app/serializers/discourse_shop/product_serializer.rb
+++
@@ -1,7 +0,0 @@
-
-# frozen_string_literal: true
-class DiscourseShop::ProductSerializer < ApplicationSerializer
-  attributes :id, :title, :description, :price_cents, :currency, :status, :image_url, :stock, :created_at, :updated_at
-
-  has_many :variants, serializer: DiscourseShop::VariantSerializer
-end
--- app/serializers/discourse_shop/variant_serializer.rb
+++
@@ -1,5 +0,0 @@
-
-# frozen_string_literal: true
-class DiscourseShop::VariantSerializer < ApplicationSerializer
-  attributes :id, :name, :price_cents, :stock
-end
