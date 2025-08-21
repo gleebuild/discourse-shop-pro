d40f5f0cbf3715b51392d8d3ebc5e5f402e0f699
gleebuild <gleebuild@gmail.com>
2025-08-21 13:47:37 +0000

    Delete app directory

--- app/serializers/discourse_shop/order_serializer.rb
+++
@@ -1,9 +0,0 @@
-
-# frozen_string_literal: true
-class DiscourseShop::OrderSerializer < ApplicationSerializer
-  attributes :id, :status, :product_id, :variant_id, :quantity,
-             :subtotal_cents, :discount_cents, :total_cents, :currency,
-             :recipient_name, :phone, :country, :province, :city, :address, :postal_code,
-             :shipping_company, :tracking_number,
-             :payment_provider, :payment_id, :created_at, :updated_at
-end
